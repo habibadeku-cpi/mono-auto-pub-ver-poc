ea9e0dd6c5571330830515a766bab597cca6a329
Habib Adeku <habib.adeku@chargepoint.com>
2023-04-26 16:03:05 +0200

    ci(taylor): release update for taylor ci

--- packages/auto-pub-poc/lib/hello-app/HelloApp.tsx
+++ packages/auto-pub-poc/lib/hello-app/HelloApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const HelloApp = (props: any) => {
 
-    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - release for taylor</div>;
+    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - release ci for taylor</div>;
   
 };
