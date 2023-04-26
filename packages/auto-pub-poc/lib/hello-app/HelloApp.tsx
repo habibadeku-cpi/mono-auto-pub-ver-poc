cf92c1d5e40d6c07666862d26c30a9ade1e56d48
Habib Adeku <habib.adeku@chargepoint.com>
2023-04-26 17:06:22 +0200

    feat(package): test auto pub feature new release

--- packages/auto-pub-poc/lib/hello-app/HelloApp.tsx
+++ packages/auto-pub-poc/lib/hello-app/HelloApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const HelloApp = (props: any) => {
 
-    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - release ci for taylor</div>;
+    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - new release ci for taylor</div>;
   
 };
