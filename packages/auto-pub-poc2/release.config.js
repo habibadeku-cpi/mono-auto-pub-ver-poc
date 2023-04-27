ab0eebd98f3da5e3659eca4cbd06cbb1f38ead54
Habib Adeku <habib.adeku@chargepoint.com>
2023-04-27 08:03:38 +0200

    feat(update): update for sr config for changelog

--- packages/auto-pub-poc2/release.config.js
+++ packages/auto-pub-poc2/release.config.js
@@ -1,33 +1,34 @@
 module.exports = {
-    tagFormat: 'libv2_${version}',
-    branches: [
-      'main',
+  tagFormat: 'libv2_${version}',
+  packages: ["packages/auto-pub-poc2/*"],
+  branches: [
+    'main',
+    {
+      name: 'beta',
+      prerelease: true
+    }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    {
+      "extends": "semantic-release-monorepo"
+    },
+    [
+      '@semantic-release/changelog',
       {
-        name: 'beta',
-        prerelease: true
+        changelogFile: 'CHANGELOG.md'
       }
     ],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
       {
-        "extends": "semantic-release-monorepo"
-      },
-      [
-        '@semantic-release/changelog',
-        {
-          changelogFile: 'CHANGELOG.md'
-        }
-      ],
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'dist/**'],
-          // eslint-disable-next-line no-template-curly-in-string
-          message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }
-      ]
-    ],
-  }
+        assets: ['packages/auto-pub-poc2/CHANGELOG.md', 'packages/auto-pub-poc2/dist/**'],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ],
+}
