dce0c4c8e9e996c7b10c0277eed30cb5d9328340
Habib Adeku <habib.adeku@chargepoint.com>
2023-04-27 09:34:03 +0200

    feat(update): update lib2

--- packages/auto-pub-poc2/lib/hello-app/HelloApp.tsx
+++ packages/auto-pub-poc2/lib/hello-app/HelloApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const HelloApp = (props: any) => {
 
-    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib 2 version new update - new check</div>;
+    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib 2 test update</div>;
   
 };
