0b4f4841ed897653554d002f1a86654af9c159b3
Habib Adeku <habib.adeku@chargepoint.com>
2023-04-27 15:54:27 +0200

    fix(update): update fixes lib1 message update to cicd

--- packages/auto-pub-poc/lib/hello-app/HelloApp.tsx
+++ packages/auto-pub-poc/lib/hello-app/HelloApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const HelloApp = (props: any) => {
 
-    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib 1 cicd new test update</div>;
+    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib 1 cicd new test updates</div>;
   
 };
